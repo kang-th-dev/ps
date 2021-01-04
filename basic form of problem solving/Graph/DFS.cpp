0b4854bb4e3b7e6007f9d0b9b4c64e2cefe21e3e
kang-th-dev <kth.dev1107@gmail.com>
2021-01-05 02:10:03 +0900

    update 2021-01-05

---
+++ basic form of problem solving/Graph/DFS.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+class graph
+{
+private:
+    int v,e;
+    bool ** connected;
+    bool * visit;
+public:
+    void push(int v1,int v2){connected[v1][v2] = true; }
+    void visited(int ee, bool isVisit){visit[ee] = isVisit;}
+    bool visited(int ee) { return visit[ee]; }
+    bool isConnected(int v1,int v2){ return connected[v1][v2];}
+    graph(int cv,int ce);
+    ~graph();
+    
+    //test
+    void print(){
+        for(int i=1; i<=v; i++){
+            for(int j=1; j<=v; j++){
+                printf("%d ",connected[i][j]);
+            }
+            cout << endl;
+        }
+    }
+    
+};
+
+//constructor
+graph::graph(int cv,int ce)
+{
+    this->v = cv;
+    this->e = ce;
+    connected = new bool* [cv+1];
+    for(int i=1; i <=cv; i++){
+        connected[i] = new bool [cv+1];
+    }
+    
+    visit = new bool[cv+1];
+    
+    //initilaze
+    for(int i=1; i <=cv; i++){        
+        visit[i] = false;
+        for(int j=1; j <=cv; j++){
+            connected[i][j] = false;
+        }
+    }
+}
+
+//destroy
+graph::~graph()
+{
+    delete[] connected;
+    delete[] visit;
+}
+
+bool dfsVisited[1001]={false,};
+bool bfsVisited[1001]={false,};
+void dfs(graph g,int ee,int v){
+    printf("%d ",ee);
+    dfsVisited[ee]= true;
+    for(int i=1; i<=v; i++){
+    if(g.isConnected(ee,i) && !dfsVisited[i]){
+            dfs(g,i, v);
+        }
+    }
+}
+
+int main(){
+        
+    int v,e;
+    //vertax&edge input
+
+    int s; 
+    scanf("%d%d%d",&v,&e,&s);
+    graph g(v,e);
+    
+    //Expressing that two vertices are connected
+    int v1,v2;
+    for(int i=0; i<e; i++){
+        cin >>v1 >>v2;
+        g.push(v1,v2);
+        g.push(v2,v1);
+    }
+    dfs(g,s,v);
+}
---
+++ basic form of problem solving/readme.md
@@ -0,0 +1,14 @@
+# PS 기본 형태
+
+> STL 같은 편법 안쓰고 구현
+
+* [Coefficient](/Coefficient)
+--미완성
+* [Graph](/Graph)
+--제작중
+* [Number Theory](/Number%20Theory)
+--미완성
+* [Relation](/Relation)
+--미완성
+* [Tree](/Tree)
+--미완성
